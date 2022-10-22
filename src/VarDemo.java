9fd59742cb4c9f778561fd45282285d3f7497117
Alex-JBE <113236614+Alex-JBE@users.noreply.github.com>
2022-10-22 11:49:30 +0200

    Variables and Literals

---
+++ src/VarDemo.java
@@ -0,0 +1,17 @@
+public class VarDemo {
+
+    public static void main(String[] args) {
+
+        byte age = 54;
+        char sex = 'f';
+        double weight = 55.2;
+        int height = 170;
+        System.out.println();
+
+        System.out.println("Персональные данные пользователя:");
+        System.out.println("Возраст: " + age);
+        System.out.println("Пол: " + sex);
+        System.out.println("Вес: " + weight);
+        System.out.println("Рост " + height);
+    }
+}
